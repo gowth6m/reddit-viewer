8a4bcbf5dadec31f8cb37846752f1e9ffec39ec2
Gowthaman Ravindrathas <gowtham4n@gmail.com>
2022-12-04 16:25:41 +0000

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,28 +1,16 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:reddit_viewer/main.dart';
 import 'package:reddit_viewer/ui/home.dart';
 
 void main() {
-
-
-      testWidgets('Q: Load tasks', (widgetTester) async {
-      await widgetTester.pumpWidget(
-        const MaterialApp(
-          home: HomePage(title: 'Testing',),
-        ),
-      );
-      await widgetTester.pumpAndSettle();
-    });
-
+  testWidgets('Q: Load Home Page', (widgetTester) async {
+    await widgetTester.pumpWidget(
+      const MaterialApp(
+        home: HomePage(),
+      ),
+    );
+    await widgetTester.pumpAndSettle();
+  });
 
   // testWidgets('Counter increments smoke test', (WidgetTester tester) async {
   //   // Build our app and trigger a frame.
